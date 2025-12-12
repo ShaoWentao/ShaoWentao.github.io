c0919ee291157c7985f3f147fc8f39b19e22266b
ShaoWentao <shaowentao191@gmail.com>
2025-12-12 11:38:55 +0800

    Create LuminaireDimensions.cs

---
+++ src/Photometric.Core/Models/Common/LuminaireDimensions.cs
@@ -0,0 +1,12 @@
+namespace Photometric.Core.Models.Common;
+
+/// <summary>
+/// Physical dimensions of a luminaire.
+/// Units are meters.
+/// </summary>
+public class LuminaireDimensions
+{
+    public double Width { get; init; }
+    public double Length { get; init; }
+    public double Height { get; init; }
+}
