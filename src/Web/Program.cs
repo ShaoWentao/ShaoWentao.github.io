21b853a0296b31fc29984f452ab18867a6961ab4
ShaoWentao <shaowentao191@gmail.com>
2025-12-12 12:27:47 +0800

    Update Program.cs

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -3,8 +3,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<Web.App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 await builder.Build().RunAsync();
-
