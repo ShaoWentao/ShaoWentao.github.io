31df2128b0e484b8b2478c6e8a18720064ec2b54
ShaoWentao <shaowentao191@gmail.com>
2025-12-12 12:25:22 +0800

    Update IesParseResult.cs

--- src/Photometric.Core/Readers/IES/IesParseResult.cs
+++ src/Photometric.Core/Readers/IES/IesParseResult.cs
@@ -1,30 +1,12 @@
-using Photometric.Core.Models;
+using Photometric.Core.Models.Common;
 using Photometric.Core.Models.IES;
 
 namespace Photometric.Core.Readers.IES;
 
-/// <summary>
-/// Result of parsing an IES file (header + angles + candela).
-/// </summary>
 public class IesParseResult
 {
-    /// <summary>
-    /// Common photometric header (shared model).
-    /// </summary>
     public PhotometricHeader CommonHeader { get; init; } = new();
-
-    /// <summary>
-    /// IES-specific header extension.
-    /// </summary>
     public IesHeader IesHeader { get; init; } = new();
-
-    /// <summary>
-    /// Angular definition (vertical & horizontal angles).
-    /// </summary>
     public PhotometricAngles Angles { get; init; } = new();
-
-    /// <summary>
-    /// Candela matrix (rows = horizontal, cols = vertical).
-    /// </summary>
     public CandelaMatrix Candela { get; init; } = new();
 }
