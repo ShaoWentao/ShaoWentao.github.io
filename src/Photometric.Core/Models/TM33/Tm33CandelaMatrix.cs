62ea2fe75b9e7a8723320f13bab81225f2703b44
ShaoWentao <shaowentao191@gmail.com>
2025-12-12 12:24:38 +0800

    Update Tm33CandelaMatrix.cs

--- src/Photometric.Core/Models/TM33/Tm33CandelaMatrix.cs
+++ src/Photometric.Core/Models/TM33/Tm33CandelaMatrix.cs
@@ -1,13 +1,8 @@
 namespace Photometric.Core.Models.TM33;
 
-/// <summary>
-/// Candela matrix for TM-33.
-/// Rows = horizontal angles, Columns = vertical angles.
-/// </summary>
 public class Tm33CandelaMatrix
 {
     public double[,] Values { get; init; } = new double[0, 0];
-
     public int HorizontalCount => Values.GetLength(0);
     public int VerticalCount => Values.GetLength(1);
 }
