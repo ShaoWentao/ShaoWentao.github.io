5ec9ce083006da23ef2ddcdb521d5eb7fcdea530
ShaoWentao <shaowentao191@gmail.com>
2025-12-12 12:23:36 +0800

    Update Tm33Header.cs

--- src/Photometric.Core/Models/TM33/Tm33Header.cs
+++ src/Photometric.Core/Models/TM33/Tm33Header.cs
@@ -1,12 +1,9 @@
 namespace Photometric.Core.Models.TM33;
 
-/// <summary>
-/// TM-33 document header.
-/// </summary>
 public class Tm33Header
 {
     public string Standard { get; init; } = "TM-33-18";
-    public string Laboratory { get; init; } = string.Empty;
-    public string ReportNumber { get; init; } = string.Empty;
-    public DateTime TestDate { get; init; }
+    public string Laboratory { get; init; } = "";
+    public string ReportNumber { get; init; } = "";
+    public DateTime? TestDate { get; init; }
 }
