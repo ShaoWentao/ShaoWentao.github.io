5c9970de09695d17c09f12d01545cde4baf2bda9
ShaoWentao <shaowentao191@gmail.com>
2025-12-12 15:41:38 +0800

    Update Program.cs

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Web;
+using MudBlazor.Services; // 添加这行
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,5 +11,6 @@ builder.Services.AddScoped(sp => new HttpClient
 {
     BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
 });
+builder.Services.AddMudServices(); // 添加这行
 
 await builder.Build().RunAsync();
