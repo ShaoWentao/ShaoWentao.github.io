17a5be435af38ff9bef78a4ece01751be5c71439
ShaoWentao <shaowentao191@gmail.com>
2025-12-12 12:24:08 +0800

    Update Tm33Photometry.cs

--- src/Photometric.Core/Models/TM33/Tm33Photometry.cs
+++ src/Photometric.Core/Models/TM33/Tm33Photometry.cs
@@ -1,16 +1,8 @@
 namespace Photometric.Core.Models.TM33;
 
-/// <summary>
-/// Photometric section of TM-33.
-/// </summary>
 public class Tm33Photometry
 {
-    /// <summary>
-    /// Photometric type (C, B, A).
-    /// </summary>
     public string Type { get; init; } = "C";
-
     public Tm33Angles Angles { get; init; } = new();
-
     public Tm33CandelaMatrix Candela { get; init; } = new();
 }
