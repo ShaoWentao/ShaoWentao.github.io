ae15a10e46a5f40786e3a42c88b5755af20553d1
ShaoWentao <shaowentao191@gmail.com>
2025-12-12 16:14:29 +0800

    Update CandelaMatrix.cs

--- src/Photometric.Core/Models/Common/CandelaMatrix.cs
+++ src/Photometric.Core/Models/Common/CandelaMatrix.cs
@@ -4,26 +4,23 @@ namespace Photometric.Core.Models.Common
 {
     public class CandelaMatrix
     {
-        // 现有的属性...
-        public List<double> VerticalAngles { get; set; }
-        public List<double> HorizontalAngles { get; set; }
-        public double[,] Values { get; set; }
+        // ✅ 修复：添加 = new() 默认值
+        public List<double> VerticalAngles { get; set; } = new();
+        public List<double> HorizontalAngles { get; set; } = new();
+        public double[,] Values { get; set; } = new double[0, 0];
 
-        // 🟢 新增以下 4 个属性以修复 CS0117 错误
         public double PeakCandela { get; set; }
-        public double PeakPlaneIndex { get; set; } // 或者叫 PeakHorizontalAngle
+        public double PeakPlaneIndex { get; set; }
         public double PeakVerticalAngle { get; set; }
         public double BeamAngle { get; set; }
 
-        // 构造函数 (确保参数类型匹配)
+        public CandelaMatrix() { }
+
         public CandelaMatrix(List<double> verticalAngles, List<double> horizontalAngles, double[,] values)
         {
             VerticalAngles = verticalAngles;
             HorizontalAngles = horizontalAngles;
             Values = values;
         }
-
-        // 如果有一个空的构造函数，也保留它
-        public CandelaMatrix() { }
     }
 }
