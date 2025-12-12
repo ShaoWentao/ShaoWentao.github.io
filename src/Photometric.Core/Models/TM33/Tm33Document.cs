548825722cf560dcf1a043dbbac2261dd225ab2f
ShaoWentao <shaowentao191@gmail.com>
2025-12-12 12:23:21 +0800

    Update Tm33Document.cs

--- src/Photometric.Core/Models/TM33/Tm33Document.cs
+++ src/Photometric.Core/Models/TM33/Tm33Document.cs
@@ -1,8 +1,5 @@
 namespace Photometric.Core.Models.TM33;
 
-/// <summary>
-/// Root object of a TM-33-18 photometric document.
-/// </summary>
 public class Tm33Document
 {
     public Tm33Header Header { get; init; } = new();
